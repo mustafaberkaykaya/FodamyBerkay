4aad39f9ec8d019985133fb5b5cb42a46fe20cc2
Berkay  Kaya <mberkaykaya@outlook.com>
2022-03-08 10:57:35 +0300

    add first screen

---
+++ Scenes/Walkthrough/WalkthroughRouter.swift
@@ -0,0 +1,8 @@
+//
+//  WalkthroughRouter.swift
+//  FodamyBerkay
+//
+//  Created by Mustafa Berkay Kaya on 7.03.2022.
+//
+
+final class WalkthroughRouter: Router {}
---
+++ Scenes/Walkthrough/WalkthroughViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WalkthroughViewController.swift
+//  FodamyBerkay
+//
+//  Created by Mustafa Berkay Kaya on 7.03.2022.
+//
+
+import UIKit
+
+final class WalkthroughViewController: BaseViewController<WalkthroughViewModel> {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
