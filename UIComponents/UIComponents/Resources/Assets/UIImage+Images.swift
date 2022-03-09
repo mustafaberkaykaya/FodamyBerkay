8eec43a9c775d34e88382e8d90bb88cafbca304d
Berkay  Kaya <mberkaykaya@outlook.com>
2022-03-09 14:02:58 +0300

    add swiftGen

---
+++ UIComponents/UIComponents/Resources/Assets/UIImage+Images.swift
@@ -0,0 +1,18 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import UIKit
+
+// swiftlint:disable identifier_name line_length nesting type_body_length type_name
+public extension UIImage {
+
+  static var cc: UIImage {
+      return Asset.Images.cc.image
+  }
+
+  static var jjj: UIImage {
+      return Asset.Images.jjj.image
+  }
+}
+// swiftlint:enable identifier_name line_length nesting type_body_length type_name
+
