d934b1500bb42eea7d8a4f140f629d539c0674dd
Berkay  Kaya <mberkaykaya@outlook.com>
2022-03-09 14:05:09 +0300

    add colors

--- UIComponents/UIComponents/Resources/Assets/Assets.swift
+++ UIComponents/UIComponents/Resources/Assets/Assets.swift
@@ -10,6 +10,8 @@
 #endif
 
 // Deprecated typealiases
+@available(*, deprecated, renamed: "ColorAsset.Color", message: "This typealias will be removed in SwiftGen 7.0")
+public typealias AssetColorTypeAlias = ColorAsset.Color
 @available(*, deprecated, renamed: "ImageAsset.Image", message: "This typealias will be removed in SwiftGen 7.0")
 public typealias AssetImageTypeAlias = ImageAsset.Image
 
@@ -20,6 +22,14 @@ public typealias AssetImageTypeAlias = ImageAsset.Image
 // swiftlint:disable identifier_name line_length nesting type_body_length type_name
 public enum Asset {
   public enum Colors {
+    public static let appPrimaryBackground = ColorAsset(name: "appPrimaryBackground")
+    public static let appSecondaryBackground = ColorAsset(name: "appSecondaryBackground")
+    public static let appCinder = ColorAsset(name: "appCinder")
+    public static let appHeather = ColorAsset(name: "appHeather")
+    public static let appRaven = ColorAsset(name: "appRaven")
+    public static let appRed = ColorAsset(name: "appRed")
+    public static let appWhite = ColorAsset(name: "appWhite")
+    public static let appZircon = ColorAsset(name: "appZircon")
   }
   public enum Icons {
   }
@@ -31,6 +41,53 @@ public enum Asset {
 // swiftlint:enable identifier_name line_length nesting type_body_length type_name
 
 // MARK: - Implementation Details
+
+public final class ColorAsset {
+  public fileprivate(set) var name: String
+
+  #if os(macOS)
+  public typealias Color = NSColor
+  #elseif os(iOS) || os(tvOS) || os(watchOS)
+  public typealias Color = UIColor
+  #endif
+
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
+  public private(set) lazy var color: Color = {
+    guard let color = Color(asset: self) else {
+      fatalError("Unable to load color asset named \(name).")
+    }
+    return color
+  }()
+
+  #if os(iOS) || os(tvOS)
+  @available(iOS 11.0, tvOS 11.0, *)
+  public func color(compatibleWith traitCollection: UITraitCollection) -> Color {
+    let bundle = BundleToken.bundle
+    guard let color = Color(named: name, in: bundle, compatibleWith: traitCollection) else {
+      fatalError("Unable to load color asset named \(name).")
+    }
+    return color
+  }
+  #endif
+
+  fileprivate init(name: String) {
+    self.name = name
+  }
+}
+
+public extension ColorAsset.Color {
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
+  convenience init?(asset: ColorAsset) {
+    let bundle = BundleToken.bundle
+    #if os(iOS) || os(tvOS)
+    self.init(named: asset.name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    self.init(named: NSColor.Name(asset.name), bundle: bundle)
+    #elseif os(watchOS)
+    self.init(named: asset.name)
+    #endif
+  }
+}
 
 public struct ImageAsset {
   public fileprivate(set) var name: String
