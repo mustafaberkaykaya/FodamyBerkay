a367776c1294a259e35f321ebe61e50bd8d48194
Berkay  Kaya <mberkaykaya@outlook.com>
2022-03-11 17:25:59 +0300

    configure uicomponents for walkthroughScreen

--- FodamyBerkay/Scenes /Walkthrough/WalkthroughViewModel.swift
+++ FodamyBerkay/Scenes /Walkthrough/WalkthroughViewModel.swift
@@ -8,6 +8,8 @@
 protocol WalkthroughViewDataSource {
     func numberOfItemsAt(section: Int) -> Int
     func cellItemAt(indexPath: IndexPath) -> WalkthroughCellProtocol
+    
+    var currentPage: Int { get }
 }
 
 protocol WalkthroughViewEventSource {}
@@ -15,6 +17,7 @@ protocol WalkthroughViewEventSource {}
 protocol WalkthroughViewProtocol: WalkthroughViewDataSource, WalkthroughViewEventSource {}
 
 final class WalkthroughViewModel: BaseViewModel<WalkthroughRouter>, WalkthroughViewProtocol {
+    var currentPage = 0
     
     func numberOfItemsAt(section: Int) -> Int {
         return cellItems.count
@@ -23,7 +26,21 @@ final class WalkthroughViewModel: BaseViewModel<WalkthroughRouter>, WalkthroughV
     func cellItemAt(indexPath: IndexPath) -> WalkthroughCellProtocol {
         return cellItems[indexPath.row]
     }
-    
-    private let cellItems: [WalkthroughCellProtocol] = []
-    
+
+    private let cellItems: [WalkthroughCellProtocol] = [WalkthroughCellModel(
+                                                            image: .imgWalkthrough1,
+                                                            titleText: L10n.WalkThrough._1Title,
+                                                            descriptionText: L10n.WalkThrough._1Description),
+                                                        WalkthroughCellModel(
+                                                            image: .imgWalkthrough2,
+                                                            titleText: L10n.WalkThrough._1Title,
+                                                            descriptionText: L10n.WalkThrough._1Description),
+                                                        WalkthroughCellModel(
+                                                            image: .imgWalkthrough3,
+                                                            titleText: L10n.WalkThrough._1Title,
+                                                            descriptionText: L10n.WalkThrough._1Description),
+                                                        WalkthroughCellModel(
+                                                            image: .imgWalkthrough4,
+                                                            titleText: L10n.WalkThrough._1Title,
+                                                            descriptionText: L10n.WalkThrough._1Description)]
 }
