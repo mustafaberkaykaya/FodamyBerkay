72ffd6438faaedefe299c086d94c16ee5b99a283
Berkay  Kaya <mberkaykaya@outlook.com>
2022-03-11 11:21:13 +0300

    add walkthroughScreen

--- FodamyBerkay/Scenes /Walkthrough/WalkthroughViewController.swift
+++ FodamyBerkay/Scenes /Walkthrough/WalkthroughViewController.swift
@@ -2,15 +2,63 @@
 //  WalkthroughViewController.swift
 //  FodamyBerkay
 //
-//  Created by Mustafa Berkay Kaya on 7.03.2022.
+//  Created by Mustafa Berkay Kaya on 11.03.2022.
 //
 
 import UIKit
 
 final class WalkthroughViewController: BaseViewController<WalkthroughViewModel> {
     
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(WalkthroughCell.self)
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .gray
     }
+    
 }
+
+// MARK: - UICollectionViewDataSource
+extension WalkthroughViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.numberOfItemsAt(section: section)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: WalkthroughCell = collectionView.dequeueReusableCell(for: indexPath)
+        let cellItem = viewModel.cellItemAt(indexPath: indexPath)
+        cell.set(viewModel: cellItem)
+        return cell
+    }
+    
+}
+
+// swiftlint:disable line_length
+// MARK: - UICollectionViewDelegateFlowLayout
+extension WalkthroughViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .zero
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return .zero
+
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return .zero
+
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .zero
+    }
+    
+}
+// swiftlint:enable line_length
--- FodamyBerkay/Scenes /Walkthrough/WalkthroughViewModel.swift
+++ FodamyBerkay/Scenes /Walkthrough/WalkthroughViewModel.swift
@@ -2,17 +2,30 @@
 //  WalkthroughViewModel.swift
 //  FodamyBerkay
 //
-//  Created by Mustafa Berkay Kaya on 7.03.2022.
+//  Created by Mustafa Berkay Kaya on 11.03.2022.
 //
 
 import Foundation
 
-protocol WalkthroughViewDataSource {}
+protocol WalkthroughViewDataSource {
+    func numberOfItemsAt(section: Int) -> Int
+    func cellItemAt(indexPath: IndexPath) -> WalkthroughCellProtocol
+}
 
 protocol WalkthroughViewEventSource {}
 
 protocol WalkthroughViewProtocol: WalkthroughViewDataSource, WalkthroughViewEventSource {}
 
 final class WalkthroughViewModel: BaseViewModel<WalkthroughRouter>, WalkthroughViewProtocol {
+    
+    func numberOfItemsAt(section: Int) -> Int {
+        return cellItems.count
+    }
+    
+    func cellItemAt(indexPath: IndexPath) -> WalkthroughCellProtocol {
+        return cellItems[indexPath.row]
+    }
+    
+    private let cellItems: [WalkthroughCellProtocol] = []
     
 }
