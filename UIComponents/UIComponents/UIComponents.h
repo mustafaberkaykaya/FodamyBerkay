88462873f4479251b1b217f404796b613e716cd6
Berkay  Kaya <mberkaykaya@outlook.com>
2022-03-11 17:25:07 +0300

    add global import

--- UIComponents/UIComponents/UIComponents.h
+++ UIComponents/UIComponents/UIComponents.h
@@ -8,6 +8,7 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import <MobilliumBuilders/MobilliumBuilders-Swift.h>
+#import <TinyConstraints/TinyConstraints-Swift.h>
 
 //! Project version number for UIComponents.
 FOUNDATION_EXPORT double UIComponentsVersionNumber;
