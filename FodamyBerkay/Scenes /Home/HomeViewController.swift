c566099532c9c6ea1fa7b41c268d7c1b21d7cf3a
Berkay  Kaya <mberkaykaya@outlook.com>
2022-03-14 14:54:17 +0300

    add navbar logo

--- FodamyBerkay/Scenes /Home/HomeViewController.swift
+++ FodamyBerkay/Scenes /Home/HomeViewController.swift
@@ -8,9 +8,25 @@
 import UIKit
 
 final class HomeViewController: BaseViewController<HomeViewModel> {
+    private let imageView = UIImageViewBuilder().build()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addSubViews()
     }
     
+}
+// MARK: - UILayout
+extension HomeViewController {
+    private func addSubViews() {
+        addNavigationBarLogo()
+    }
+   
+    private func addNavigationBarLogo() {
+        imageView.image = UIImage.imgLogoFodamy
+        navigationItem.titleView = imageView
+        imageView.contentMode = .scaleAspectFit
+        imageView.width(110)
+        imageView.height(30)
+    }
 }
