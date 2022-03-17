2f971097a3445bc4851ef00fe92144a3e8f00e7d
Berkay  Kaya <mberkaykaya@outlook.com>
2022-03-17 13:57:05 +0300

    configure Extensions

--- UIComponents/UIComponents/Extensions/ReusableView+Extensions.swift
+++ UIComponents/UIComponents/Extensions/ReusableView+Extensions.swift
@@ -5,8 +5,12 @@
 //  Created by Mustafa Berkay Kaya on 11.03.2022.
 //
 
+public protocol ReusableView: AnyObject {
+    static var reuseIdentifier: String { get }
+}
+
 public extension ReusableView where Self: UIView {
-    static var defaultReuseIdentifier: String {
+    static var reuseIdentifier: String {
         return String(describing: self)
     }
 }
