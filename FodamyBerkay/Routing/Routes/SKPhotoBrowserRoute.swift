b825f8be7da08c0bf6b3dbe3f4a9eeca6498ae49
Berkay  Kaya <mberkaykaya@outlook.com>
2022-03-28 22:39:24 +0300

    add new folders

--- FodamyBerkay/Routing/Routes/SKPhotoBrowserRoute.swift
+++ FodamyBerkay/Routing/Routes/SKPhotoBrowserRoute.swift
@@ -12,12 +12,10 @@ protocol SKPhotoBrowserRoute {
 }
 
 extension SKPhotoBrowserRoute where Self: RouterProtocol {
-    
     func presentSKPhotoBrowser(with photos: [String], viewController: UIViewController, initialPageIndex: Int, delegate: PhotoBrowserDelegate) {
         let skPhotos = photos.map { SKPhoto.photoWithImageURL($0) }
         let photoBrowser = SKPhotoBrowser(photos: skPhotos, initialPageIndex: initialPageIndex)
         photoBrowser.delegate = delegate
         viewController.present(photoBrowser, animated: true)
     }
-    
 }
